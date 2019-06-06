b643e08650aa2a923c911e8c1e850e5209cf8c75
HimanshuP90 <himanshup895@gmail.com>
2019-06-06 22:49:10 +0530

    react added

--- pages/post.js
+++ pages/post.js
@@ -1,12 +1,31 @@
-import Layout from  '../components/Layout';
-import { withRouter } from 'next/router'
+import Layout from "../components/Layout";
+import { withRouter } from "next/router";
 
 const Post = ({ router }) => (
-    <Layout title={router.query.title}>
-        <p style={{ width: "80vw" }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est expedita voluptatibus quaerat, asperiores, vitae mollitia tempore provident explicabo voluptas autem beatae in, nisi reiciendis error tenetur ratione vero aperiam eos!
-        Id itaque consequuntur, voluptatum illo qui tempora veniam ea unde tempore mollitia harum facere dolore ipsam, magni aspernatur, eius ullam commodi suscipit porro architecto fuga corporis dolorem minus. Dolorum, itaque.
-        Fugiat libero odit aliquam facilis dolor, eius similique adipisci voluptas iure voluptatibus aliquid nemo temporibus praesentium perferendis, odio provident quisquam ex! Aliquid, reiciendis! Quia porro rerum, fugiat tempora facere neque.</p>
-    </Layout>
+  <Layout title={router.query.title}>
+    <div  style={{ width: "80vw" }}>
+    <p><b>1) Initialization:</b> setup props and state</p>
+    <p><b>2) Mounting:</b> componentWillMount (Immediately before inital rendering)
+      render ( return one of the following types: React elements, String and
+      numbers, Portals, Booleans, null, ) componentDidMount (Immediately after
+      inital rendering)
+    </p>
+    <p><b>3) Updation: </b> props componentWillReceiveProps (When component receives new
+      props) shouldComponentUpdate (Before rendering, after receiving new props
+      or state) componentWillupdate (Before rendering, after receiving new props
+      or state) render componentDidUpdate (After component's updates are flushed
+      to DOM) state shouldComponentUpdate( is shouldComponentUpdate return false
+      then render() will not be invoked) componentWillupdate render
+      componentDidUpdate
+    </p>
+
+    <p><b>4) Unmounting: </b> componentWillUnmount (Immediately before removing component
+      from DOM)
+    </p>
+
+    <p><b>5): </b> componentDidCatch(error, info) </p>
+    </div>
+  </Layout>
 );
 
 export default withRouter(Post);
